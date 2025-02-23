885f70518ca343df39238f4ed6efd59bf2fdbcab
Ayan66075 <ayanzameer960@gmail.com>
2025-02-23 18:04:58 +0500

    app.js added

---
+++ app.js
@@ -0,0 +1,40 @@
+let allUsers = []
+
+let users = localStorage.getItem('users')
+
+if(users !== null){
+    allUsers = JSON.parse(users)
+}
+
+function signup(){
+    let name = document.getElementById('name')
+    let email = document.getElementById('email')
+    let password = document.getElementById('password')
+
+    let user = {
+        name : name.value,
+        email : email.value,
+        password : password.value
+    }
+
+    allUsers.push(user)
+
+    localStorage.setItem('users', JSON.stringify(allUsers))
+}
+
+function login(){
+    let email = document.getElementById('email')
+    let password = document.getElementById('password')
+
+   let filterUser = allUsers.filter(function(data){
+    return data.email == email.value && data.password == password.value
+   })
+
+   if(filterUser.length){
+    alert('Login Success')
+   }
+   else{
+    alert('Incorrect Email/Password')
+   }
+   
+}
